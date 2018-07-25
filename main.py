ab5b77ace6a8d5ee1f6a33997a099119433df893
GajananThenge <38664939+GajananThenge@users.noreply.github.com>
2018-07-25 15:08:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,60 +7,130 @@ Created on Sun Jun 17 18:11:47 2018
 
 import sqlite3 
 import pandas as pd
-from sklearn.tree import DecisionTreeRegressor 
-from sklearn.linear_model import LinearRegression 
+ 
 from sklearn.model_selection import train_test_split 
 from sklearn.metrics import mean_squared_error 
 from math import sqrt
+import numpy as np
 # Create your connection. 
-try:
-    cnx = sqlite3.connect('data\database.sqlite') 
+
+
+def get_feature_columns():
+    return ['overall_rating', 'potential', 'preferred_foot',
+            'defensive_work_rate', 'crossing',
+           'finishing', 'heading_accuracy', 'short_passing',
+           'dribbling',  'free_kick_accuracy', 'long_passing',
+           'ball_control', 'acceleration', 'sprint_speed',
+           'reactions','shot_power','stamina',
+           'strength', 'long_shots', 'aggression', 'interceptions',
+           'positioning','penalties', 'marking', 'standing_tackle',
+           'gk_diving', 'gk_handling', 'gk_kicking',
+           'gk_positioning', 'gk_reflexes']
+
+def get_data(sql_lite_db_path,cnx):
     df = pd.read_sql_query("SELECT * FROM Player_Attributes", cnx)
-    dataset = df.loc[:,['overall_rating','potential', 'short_passing', 'long_passing', 'volleys','dribbling',
-       'ball_control','reactions', 'shot_power','strength', 'long_shots', 'aggression', 'interceptions',
-       'positioning', 'vision', 'penalties']]
+    print("=================Columns===================")
+#    print(list(df.columns))
+#    dataset = df.loc[:,['overall_rating','potential', 'short_passing', 'long_passing', 'volleys','dribbling',
+#       'ball_control','reactions', 'shot_power','strength', 'long_shots', 'aggression', 'interceptions',
+#       'positioning', 'vision', 'penalties','overall_rating']]
     
-    dataset.dropna(inplace=True)
-    
-    # count the number of NaN values in each column
-    print(dataset.isnull().sum())
-    #preferred_foot [Right,left]
-#    attacking_work_rate [high,medium,None ]
-#    defensive_work_rate [medium,low] 
-    
-    '''['id', 'player_fifa_api_id', 'player_api_id', 'date',
-       'overall_rating', 'potential', 'preferred_foot',
-       'attacking_work_rate', 'defensive_work_rate', 'crossing',
-       'finishing', 'heading_accuracy', 'short_passing', 'volleys',
-       'dribbling', 'curve', 'free_kick_accuracy', 'long_passing',
-       'ball_control', 'acceleration', 'sprint_speed', 'agility',
-       'reactions', 'balance', 'shot_power', 'jumping', 'stamina',
-       'strength', 'long_shots', 'aggression', 'interceptions',
-       'positioning', 'vision', 'penalties', 'marking', 'standing_tackle',
-       'sliding_tackle', 'gk_diving', 'gk_handling', 'gk_kicking',
-       'gk_positioning', 'gk_reflexes']'''
-    
-    X = dataset[['potential', 'short_passing', 'long_passing',
-       'ball_control','reactions', 'shot_power','strength', 'long_shots', 'volleys','dribbling','aggression', 'interceptions',
-       'positioning', 'vision', 'penalties']].values
-    
-    y = dataset['overall_rating'].values
-    
-     #Splittinvg thedataset into train_test_split
-    from sklearn.model_selection import train_test_split
-    X_train,X_test,y_train,y_test =train_test_split(X,y,test_size=.25,random_state=0)
-    
-    
+    return df[get_feature_columns()]#dataset
+
+
+
+def drop_na(df):
+    return df[df.overall_rating.notnull()]
+
+def get_null_counts(df):
+    return (df.isnull().sum())
+
+
+
+def create_fit_model(X_train,y_train):
+    from sklearn.tree import DecisionTreeRegressor 
+    from sklearn.linear_model import LinearRegression
     model = LinearRegression()
     model.fit(X_train,y_train)
-    y_pred = model.predict(X_test)
+    return model
+
+def predict(model,X_test):
+    model.predict(X_test)
     
-    resulf_df = pd.DataFrame({'Actual':y_test,'Predicted':y_pred})
-    print(model.score(X_test,y_test))
     
-except Exception as e:
-    print(e)
+def get_train_test_data(df):
+    from sklearn.model_selection import train_test_split
+    X = df.iloc[:,1:].values
+    y = dataset_1.iloc[:,0].values
     
-finally:
-    print("Closing Db Connection")
-    cnx.close()
+     #Splittinvg thedataset into train_test_split
+    X_train,X_test,y_train,y_test =train_test_split(X,y,test_size=.25,random_state=0)
+    return X_train,X_test,y_train,y_test
+
+def calc_train_error(X_train, y_train, model):
+    '''returns in-sample error for already fit model.'''
+    predictions = model.predict(X_train)
+    mse = mean_squared_error(y_train, predictions)
+    rmse = np.sqrt(mse)
+    return mse
+    
+def calc_validation_error(X_test, y_test, model):
+    '''returns out-of-sample error for already fit model.'''
+    predictions = model.predict(X_test)
+    mse = mean_squared_error(y_test, predictions)
+    rmse = np.sqrt(mse)
+    return mse
+
+if __name__=='__main__':
+    try:
+        sql_lite_db_path= r'data\database.sqlite'
+        cnx = sqlite3.connect(sql_lite_db_path) 
+        dataset = get_data(sql_lite_db_path,cnx)
+        
+        # Drop the rows having overall_rating is null
+        dataset_1 = drop_na(dataset)
+        
+        # count the number of NaN values in each column
+#        print(get_null_counts(dataset_1))63789
+        
+        
+# =============================================================================
+#         Get list of non numeric columns and convert it them into numeric
+# =============================================================================
+        not_num_cols=dataset_1.select_dtypes(exclude = [np.number,np.int16,np.bool,np.float32] )
+    
+        for col in not_num_cols:
+            dataset_1.loc[:,col] = dataset_1.loc[:,col].astype('category')
+            dataset_1.loc[:,col] = dataset_1.loc[:,col].cat.codes
+        
+        
+# =============================================================================
+#         Get train and test data
+# =============================================================================
+        X_train,X_test,y_train,y_test = get_train_test_data(dataset_1)
+        
+# =============================================================================
+#         Model creation and model fitting
+# =============================================================================
+        model = create_fit_model(X_train,y_train)
+        
+# =============================================================================
+#         Predict the values 
+# =============================================================================
+        #y_pred = predict(model,X_test)
+        mse = calc_train_error(X_train, y_train, model)
+        print("Mean squared error on train data: {}".format(mse))
+        mse = calc_validation_error(X_test, y_test, model)
+        print("Mean squared error on test data: {}".format(mse))
+        
+        
+#        #Calculate Accuracy of Model
+#        print(model.score(X_test,y_test))
+#    #    resulf_df = pd.DataFrame({'Actual':y_test,'Predicted':y_pred})
+        
+    except Exception as e:
+        print(e)
+        
+    finally:
+        print("Closing Db Connection")
+        cnx.close()
